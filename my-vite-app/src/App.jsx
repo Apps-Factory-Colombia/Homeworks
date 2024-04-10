30f1ac757665913b47cf7d0fe6afa7611014ccce
Apps Factory <appsfactorycolombia@gmail.com>
2024-04-10 12:00:00 -0500

    challenge 08

--- my-vite-app/src/App.jsx
+++ my-vite-app/src/App.jsx
@@ -6,6 +6,7 @@ const bookStack = new Stack();
 
 bookStack.push({ name: '1984', isbn: '1234567890', author: 'George Orwell', editorial: 'Secker & Warburg' });
 bookStack.push({ name: 'El Principito', isbn: '1122334455', author: 'Antoine de Saint-Exupéry', editorial: 'Reynal & Hitchcock' });
+bookStack.push({ name: 'El Principito 2', isbn: '112233445125', author: 'Antoine de Saint-Exupéry', editorial: 'Reynal & Hitchcock' });
 
 function App() {
   const [books, setBooks] = useState(bookStack.print());
