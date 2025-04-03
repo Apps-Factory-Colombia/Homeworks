efc8c00a2d64190f14a5dd1ee6c85f52351b0049
Apps Factory <appsfactorycolombia@gmail.com>
2025-04-03 08:55:48 -0500

    challenge_07

--- my-vite-app/src/App.jsx
+++ my-vite-app/src/App.jsx
@@ -1,33 +1,144 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import Navbar from "./components/Navbar";
-import Login from "./pages/Login";
-import Explore from "./pages/Explore";
-import Profile from "./pages/Profile";
-import { useAuth } from "./context/AuthContext";
-import PrivateRoute from "./routes/PrivateRoute";
-import PublicRoute from "./routes/PublicRoute";
+import React, { useState } from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
-function App() {
-  const { user } = useAuth();
+// Singly Linked List Node
+class SongNode {
+  constructor(song) {
+    this.song = song;
+    this.next = null;
+  }
+}
+
+class SongLinkedList {
+  constructor() {
+    this.head = null;
+    this.current = null;
+  }
+
+  addSong(song) {
+    const newNode = new SongNode(song);
+    if (!this.head) {
+      this.head = newNode;
+      this.current = this.head;
+    } else {
+      let temp = this.head;
+      while (temp.next) temp = temp.next;
+      temp.next = newNode;
+    }
+  }
+
+  nextSong() {
+    if (this.current && this.current.next) {
+      this.current = this.current.next;
+    }
+  }
+}
+
+const songList = new SongLinkedList();
+["Song A", "Song B", "Song C", "Song D"].forEach((s) => songList.addSong(s));
+
+const LinkedListPage = () => {
+  const [currentSong, setCurrentSong] = useState(songList.current.song);
+
+  const playNext = () => {
+    songList.nextSong();
+    setCurrentSong(songList.current.song);
+  };
+
+  return (
+    <div className="p-6 text-center">
+      <h2 className="text-2xl">Now Playing: {currentSong}</h2>
+      <button onClick={playNext} className="mt-4 px-4 py-2 bg-blue-500 text-white rounded">
+        Next Song
+      </button>
+    </div>
+  );
+};
+
+// Doubly Linked List Node
+class PageNode {
+  constructor(url) {
+    this.url = url;
+    this.next = null;
+    this.prev = null;
+  }
+}
+
+class BrowserHistory {
+  constructor() {
+    this.head = null;
+    this.current = null;
+  }
+
+  visit(url) {
+    const newNode = new PageNode(url);
+    if (this.current) {
+      newNode.prev = this.current;
+      this.current.next = newNode;
+    }
+    this.current = newNode;
+    if (!this.head) this.head = newNode;
+  }
+
+  goBack() {
+    if (this.current && this.current.prev) {
+      this.current = this.current.prev;
+    }
+  }
+
+  goForward() {
+    if (this.current && this.current.next) {
+      this.current = this.current.next;
+    }
+  }
+}
+
+const history = new BrowserHistory();
+["Home", "About", "Services", "Contact"].forEach((url) => history.visit(url));
+
+const DoublyLinkedListPage = () => {
+  const [currentPage, setCurrentPage] = useState(history.current.url);
+
+  const goBack = () => {
+    history.goBack();
+    setCurrentPage(history.current.url);
+  };
+
+  const goForward = () => {
+    history.goForward();
+    setCurrentPage(history.current.url);
+  };
+
+  return (
+    <div className="p-6 text-center">
+      <h2 className="text-2xl">Current Page: {currentPage}</h2>
+      <div className="mt-4 flex justify-center gap-4">
+        <button onClick={goBack} className="px-4 py-2 bg-red-500 text-white rounded">
+          Back
+        </button>
+        <button onClick={goForward} className="px-4 py-2 bg-green-500 text-white rounded">
+          Forward
+        </button>
+      </div>
+    </div>
+  );
+};
+
+const App = () => {
   return (
     <Router>
-      {user && <Navbar />}
-      <Routes>
-        {/* Rutas normales (solo pa cuando no se está loggeado) */}
-        <Route element={<PublicRoute />}>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-        </Route>
-
-        {/* Rutas protegidas (están dentro como child routes) */}
-        <Route element={<PrivateRoute />}>
-          <Route path="/explore" element={<Explore />} />
-          <Route path="/profile" element={<Profile />} />
-        </Route>
-      </Routes>
+      <div className="p-6">
+        <nav className="mb-6 flex justify-center gap-6">
+          <Link to="/linked-list" className="text-blue-500">Songs</Link>
+          <Link to="/doubly-linked-list" className="text-blue-500">Browser History</Link>
+        </nav>
+        <Routes>
+          <Route path="/linked-list" element={<LinkedListPage />} />
+          <Route path="/doubly-linked-list" element={<DoublyLinkedListPage />} />
+        </Routes>
+      </div>
     </Router>
   );
-}
+};
 
 export default App;
